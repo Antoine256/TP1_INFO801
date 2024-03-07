f896ff9eb491e7fdc236a6f1de6deb9bd34b4c30
Elyroma <elynebeaudeau@orange.fr>
2024-03-07 16:19:08 +0100

    test

--- agents/pompe/pompe.go
+++ agents/pompe/pompe.go
@@ -1,1 +1,13 @@
 package pompe
+
+import (
+	. "github.com/pspaces/gospace"
+)
+
+func Pompe(ts *Space, etat string) {
+	var pompe string
+	select {
+	case ts.Get("activation_pompe"):
+		Pompe(ts, "activée")
+	}
+}
