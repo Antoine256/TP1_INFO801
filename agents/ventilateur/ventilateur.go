d903d4da1d7f501f20cae9cc936ed6e6e55d6f51
Elyroma <elynebeaudeau@orange.fr>
2024-03-08 09:06:27 +0100

    main & ventilateur

--- agents/ventilateur/ventilateur.go
+++ agents/ventilateur/ventilateur.go
@@ -1,1 +1,20 @@
 package ventilateur
+
+import (
+	"fmt"
+	. "github.com/pspaces/gospace"
+)
+
+func Ventilateur(ts *Space, etat string) {
+	_, errActivation := ts.Get("activation_ventilateur")
+	_, errDesactivation := ts.Get("desactivation_ventilateur")
+	if errActivation == nil {
+		fmt.Println("Ventilateur activée")
+		Ventilateur(ts, "activée")
+	}
+	if errDesactivation == nil {
+		fmt.Println("Ventilateur désactivée")
+		Ventilateur(ts, "désactivée")
+	}
+	Ventilateur(ts, etat)
+}
--- main.go
+++ main.go
@@ -1,31 +1,27 @@
 package main
 
 import (
-	"TP1_INFO801/agents/gaz"
+	"TP1_INFO801/agents"
 	"TP1_INFO801/agents/capteur"
+	"TP1_INFO801/agents/gaz"
 	"TP1_INFO801/agents/pompe"
-	"fmt"
+	"TP1_INFO801/agents/ventilateur"
 )
 import . "github.com/pspaces/gospace"
 
 func main() {
 	var etatPompe string
+	var etatVentilateur string
 	ts := NewSpace("ts")
 
-	go gaz.Surveillance_gaz_haut(&ts, 50, 50)
-	go gaz.H2o_bas(&ts, 50)
 	go capteur.Capteur_ch4(&ts)
 	go capteur.Capteur_co(&ts)
 	go capteur.Capteur_h2o(&ts)
+	go gaz.Surveillance_gaz_haut(&ts, 50.0, 50.0)
+	go gaz.Gaz_bas(&ts, 50.0, 50.0)
+	go gaz.H2o_bas(&ts, 50.0)
+	go gaz.H2o_haut(&ts, 50.0)
 	go pompe.Pompe(&ts, etatPompe)
-
-	// Put a message into the space.
-	ts.Put("Hello world!")
-
-	// Get a message from the space
-	// via pattern matching.
-	var message string
-	t, _ := ts.Get(&message)
-
-	fmt.Println((t.GetFieldAt(0)).(string))
+	go ventilateur.Ventilateur(&ts, etatVentilateur)
+	go agents.Commande_pompe_ventilateur(&ts, 50.0, 50.0)
 }
