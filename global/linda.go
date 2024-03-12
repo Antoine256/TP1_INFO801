107590f1565f041b8805ac305314707dfa265c65
Elyroma <elynebeaudeau@orange.fr>
2024-03-12 13:36:25 +0100

    fix Add()

--- global/linda.go
+++ global/linda.go
@@ -5,8 +5,8 @@ import (
 )
 
 func Add[T any](ts *Space, name string, value T) {
-	var x float64
+	var x T
 	ts.Put(name, value) // On initialise le tuple si jamais il n'existe pas
-	ts.Get(name, &x)    // On récupère le tuple
+	ts.GetAll(name, &x) // On récupère tous les tuples pour être sûr de ne pas avoir de doublons
 	ts.Put(name, value) // On met à jour la nouvelle valeur
 }
